198cc0ec7a523279a17f74708c270be7ac3909bc
Alex <kuepper.alexander@web.de>
2017-10-03 19:06:25 +0200

    switchyear

--- Main.cpp
+++ Main.cpp
@@ -7,18 +7,38 @@ int main() {
 	int day;
 	int getday;
 	int trueDate;
+	int year;
+	bool switchyear;
+	
+	//Input
+
 	cout << "Geben Sie den Tag ein: ";
 	cin >> getday;
 	cout << "Geben Sie den Monat ein: ";
 	cin >> monat;
+	cout << "Geben Sie ein Jahr ein: ";
+	cin >> year;
+
+	// Processing 
+
+	if (year % 4 == 0) {
+		switchyear = true;
+	}
+	else {
+		switchyear = false;
+	}
+
 	if (monat <= 7 && monat != 2 && monat%2 != 0 && monat > 0) {
 		day = 31;
 	}
 	else if (monat <= 7 && monat != 2 && monat % 2 == 0 && monat > 0) {
 		day = 30;
 	}
-	else if (monat == 2) {
+	else if (monat == 2 && !switchyear) {
 		day = 29;
+	}
+	else if (monat == 2 && switchyear) {
+		day = 28;
 	}
 	else if (monat > 7 && monat % 2 == 0 && monat < 13) {
 		day = 31;
@@ -30,7 +50,10 @@ int main() {
 		day = 0;
 	}
 
-	//Ausgabe
+
+
+	//Output
+
 	cout << "Letzer Tag: " << day << endl;
 	if (getday > 0 && getday <= day) {
 		cout << "Das Datum ist richtig" << endl;
